9c2e591ba9580d7a230134f883be673f4aca5899
Arthur Fernandes Vieira <arthur.fv324@gmail.com>
2025-05-22 00:29:59 -0300

    Game repository, service and controller

---
+++ src/main/java/com/example/dslist/controller/GameController.java
@@ -0,0 +1,25 @@
+package com.example.dslist.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dslist.dto.GameMinDTO;
+import com.example.dslist.repositories.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+    
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
